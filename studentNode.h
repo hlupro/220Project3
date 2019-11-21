6c9efe1ed9901013261331a323d08beb8ec7387d
hlupro <58021415+hlupro@users.noreply.github.com>
2019-11-21 14:46:18 -0500

    Add files via upload

--- studentNode.h
+++ studentNode.h
@@ -1,31 +1,30 @@
 #ifndef STUDENTNODE_H
 #define STUDENTNODE_H
 
-#include "studentClass.h" //name is subject to change
+#include "student.h" //name is subject to change
 
 template <typename T>
-class STUDENTNODE_H
+class studentNode
 {
-private:
-  studentClass *next; //Node pointer that points to the next node in the list
-  studentClass *prev; //Node pointer that points to the prev node in the list
-  T student;          //student will hold an object of type T, which in this case should be a student object.
-
 public:
+  studentNode<T> *next; //Node pointer that points to the next node in the list
+  studentNode<T> *prev; //Node pointer that points to the prev node in the list
+  T student;            //student will hold an object of type T, which in this case should be a student object.
+
   studentClass()  //defualt constructor
   {
     next = NULL;
     prev = NULL;
-    student();
+    student = Student();
   }
   studentClass(studentClass *prevNode, studentClass *nextNode, T value)
   {
     prev = prevNode;
     next = nextNode;
     student = value; //Will need to create a copy constructor in studentClass.h
-  }  //non-default constructor will take in two pointers
-     //order to insert a new node between them
-     //May have to include the list as a parameter Will need to test
+  }                  //non-default constructor will take in two pointers
+                     //order to insert a new node between them
+                     //May have to include the list as a parameter Will need to test
 };
 
 #endif
