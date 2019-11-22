7a06c3b0cd81620d23290d57d69d0a70a70ae1ab
hlupro <58021415+hlupro@users.noreply.github.com>
2019-11-21 20:20:38 -0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -2,13 +2,27 @@
 #include "studentList.h"
 #include <fstream>
 
+void displayMenu();
+
 int main()
 {
   ifstream dataFile;
-  studentList<studentNode> list;
+  StudentList<Student> list;
   std::cout << "Reading from file" << std::endl;
+  displayMenu();
+};
 
-
-
-
+void displayMenu()
+{
+  using namespace std;
+  cout << "==============================" << endl;
+  cout << "Student Information Database Menu" << endl;
+  cout << "==============================" << endl;
+  cout << "V - View Student Information Database" << endl;
+  cout << "F - Find Student Information by Student" << endl;
+  cout << "S - Sort Student" << endl;
+  cout << "A - Add a Student" << endl;
+  cout << "D - Delete a Student" << endl;
+  cout << "Q - Quit the Program" << endl;
+  cout << "==============================" << endl;
 }
