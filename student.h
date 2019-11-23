61f4c2fa0c81b3e56351265330212f49b0191989
kutteraaron <58057197+kutteraaron@users.noreply.github.com>
2019-11-23 13:57:33 -0500

    Add files via upload

--- student.h
+++ student.h
@@ -18,11 +18,20 @@ private:
 public:
 	Student();
 	Student(string first, string last, int num, int cred, int yr, float points);
+	Student(const Student& user);
 	void read_name(string, string);
 	void read_id(int);
 	void read_credits(int);
 	void read_years(int);
 	void read_gpa(float);
+	
+	string get_lastname() const;
+	string get_firstname() const;
+	int get_id() const;
+	int get_credits() const;
+	int get_years() const;
+	float get_gpa() const;
+	
 	void estimated_year() const;
 	void display_info() const;
 };
@@ -40,15 +49,41 @@ Student::Student()
 
 Student::Student(Student student) //Will need to create getter functions for a copy constructor
 {
-	firstname = student.get;
-	lastname = last;
+	firstname = student.get_firstname();
+	lastname = student.get_lastname();
 	fullname = firstname + " " + lastname;
-	id = num;
-	years = yr;
-	GPA = points;
-	credits = cred;
+	id = student.get_id();
+	years = student.get_years();
+	GPA = student.get_gpa();
+	credits = student.get_credits();
 }
 
+string Student::get_firstname() const
+{
+	return firstname;
+}
+
+string Student::get_lastname() const
+{
+	return lastname;
+}
+
+int Student::get_id() const
+{
+	return id;
+}
+int Student::get_credits() const
+{
+	return credits;
+}
+int Student::get_years() const
+{
+	return years;
+}
+float Student::get_gpa() const
+{
+	return GPA;
+}
 
 void Student::read_name(string userfName, string userlName)
 {
@@ -64,7 +99,7 @@ void Student::read_id(int userID)
 
 void Student::read_credits(int userCredits)
 {
-  credits = userCredits;
+	credits = userCredits;
 }
 
 void Student::read_years(int userYears)
@@ -79,16 +114,26 @@ void Student::read_gpa(float userGPA)
 
 void Student::estimated_year() const
 {
-  if (credits <= 30)
-    { cout << "Freshman" << endl; }
-  if (credits <= 60 && credits >= 31)
-    { cout << "Sophomore" << endl; }
-  if (credits <= 90 && credits >= 61)
-    { cout << "Junior" << endl; }
-  else if (credits <= 120 && credits >= 91)
-    { cout << "Senior" << endl; }
-  else
-    { cout << "Undefined" << endl; }
+	if (credits <= 30)
+	{
+		cout << "Freshman" << endl;
+	}
+	if (credits <= 60 && credits >= 31)
+	{
+		cout << "Sophomore" << endl;
+	}
+	if (credits <= 90 && credits >= 61)
+	{
+		cout << "Junior" << endl;
+	}
+	else if (credits <= 120 && credits >= 91)
+	{
+		cout << "Senior" << endl;
+	}
+	else
+	{
+		cout << "Undefined" << endl;
+	}
 }
 
 void Student::display_info() const
