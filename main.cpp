093fb1d5c8ca7921aa4468acd3dcf5fea61d8164
kutteraaron <58057197+kutteraaron@users.noreply.github.com>
2019-11-25 14:35:25 -0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,4 +1,3 @@
-
 #include "studentList.h"
 #include <fstream>
 
@@ -7,87 +6,93 @@ StudentList<Student> readFile(StudentList<Student> list);
 
 int main()
 {
-  char quit = 'z';
-  std::cout << "Welcome to Kutter and Hunter's Student Information Database!" << std::endl;
-  std::cout << "Now uploading student profiles from a file." << std::endl;
-  StudentList<Student> list;
-  list = readFile(list);
-  list.displayList();
-  while (quit != 'Q')
-  {
-    using namespace std;
-    cout << "Please choose an option from the menu." << endl;
-    displayMenu();
-    cin >> quit;
+	char quit = 'z';
+	std::cout << "Welcome to Kutter and Hunter's Student Information Database!" << std::endl;
+	std::cout << "Now uploading student profiles from a file." << std::endl;
+	StudentList<Student> list;
+	list = readFile(list);
+	list.displayList();
+	while (quit != 'Q')
+	{
+		using namespace std;
+		cout << "Please choose an option from the menu." << endl;
+		displayMenu();
+		cin >> quit;
 
-    switch (quit)
-    {
-      case 'V':
-      cout << "\nAccessing Student Information Database." << endl;
-      list.displayList();
-      break;
-    case 'F':
-    {
-      cout << "Please enter the fullname of the student you would like to look up." << std::endl;
-      cin.ignore();
-      string name;
-      getline(cin,name);
-      list.searchStudent(name);
-      break;
-    }
-    case 'S':
-      list.sortList();
-      break;
-    case 'A':
-      cout << "You are now adding a student" << endl;
-      // Use insert node function
-      break;
-    case 'D':
-      cout << "You are now deleting a student." << endl;
-      // Use delete node function
-      break;
-    case 'Q':
-      cout << "You are now quiting the program." << endl;
-      break;
-    default:
-      cout << "Error, no useable input given!" << endl;
-      break;
-    }
-  }
+		switch (quit)
+		{
+		case 'V':
+			cout << "\nAccessing Student Information Database." << endl;
+			list.displayList();
+			break;
+		case 'F':
+		{
+			cout << "Please enter the fullname of the student you would like to look up." << std::endl;
+			cin.ignore();
+			string name;
+			getline(cin, name);
+			list.searchStudent(name);
+			break;
+		}
+		case 'S':
+			list.sortList();
+			break;
+		case 'A':
+			cout << "You are now adding a student" << endl;
+			// Use insert node function
+			break;
+		case 'D':
+		{
+			cout << "You are now deleting a student." << endl;
+			cout << "Enter the name of the student you want to delete." << std::endl;
+			cin.ignore();
+			string delName;
+			getline(cin, delName);
+			list.removeStudent(delName);
+			break;
+		}
+		case 'Q':
+			cout << "You are now quiting the program." << endl;
+			break;
+		default:
+			cout << "Error, no useable input given!" << endl;
+			break;
+		}
+	}
 };
 
 void displayMenu()
 {
-  using namespace std;
-  cout << "==============================" << endl;
-  cout << "Student Information Database Menu" << endl;
-  cout << "==============================" << endl;
-  cout << "V - View Student Information Database" << endl;
-  cout << "F - Find Student Information by Student" << endl;
-  cout << "S - Sort Student" << endl;
-  cout << "A - Add a Student" << endl;
-  cout << "D - Delete a Student" << endl;
-  cout << "Q - Quit the Program" << endl;
-  cout << "==============================" << endl;
+	using namespace std;
+	cout << "==============================" << endl;
+	cout << "Student Information Database Menu" << endl;
+	cout << "==============================" << endl;
+	cout << "V - View Student Information Database" << endl;
+	cout << "F - Find Student Information by Student" << endl;
+	cout << "S - Sort Student" << endl;
+	cout << "A - Add a Student" << endl;
+	cout << "D - Delete a Student" << endl;
+	cout << "Q - Quit the Program" << endl;
+	cout << "==============================" << endl;
 }
 
 StudentList<Student> readFile(StudentList<Student> list)
 {
-  int yr;
+	int yr;
 	string first;
 	string last;
 	int num = 0;
 	float points = 0;
 	int cred = 0;
-  fstream inputFile;
-  std::cout << "Now reading from file..." << std::endl;
-  inputFile.open("database", ios::in);
-  while (inputFile >> first >> last >> num >> cred >> yr >> points)
-  {
-      Student newStudent(first,last, num, cred, yr, points);
-      list.insertStudent(newStudent);
-  }
-  std::cout << "All students uploaded, now closing file...\n" << std::endl;
-  inputFile.close();
-  return list;
+	fstream inputFile;
+	std::cout << "Now reading from file..." << std::endl;
+	inputFile.open("database.txt", ios::in);
+	while (inputFile >> first >> last >> num >> cred >> yr >> points)
+	{
+		Student newStudent(first, last, num, cred, yr, points);
+		list.insertStudent(newStudent);
+	}
+	std::cout << "All students uploaded, now closing file...\n" << std::endl;
+	inputFile.close();
+	return list;
 }
